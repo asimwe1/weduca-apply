d90915ce866b8300674a5eac9432296a6a23a7eb
Salim Rutaganda <rutagandasalim@gmail.com>
2025-02-17 12:19:24 +0200

    Update study destinations images and remove flag attributes

--- src/components/Studydest.jsx
+++ src/components/Studydest.jsx
@@ -4,25 +4,21 @@ const destinations = [
   {
     title: "Study In The UK",
     image: "https://s3-alpha-sig.figma.com/img/65b3/d8b6/37aa0d2bcde6e2173e71da34a4271092?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=PAZK7bdU6gjkT3~OTqfayR4~oTWnz5h1CuCODyIe5eEfBE5dZqxLNKLVqCqbALz6PGPWfFlfHXVKSfw6P9mEkCF1j1F9D5GbQ~C2nyI3OUh9IPONkaAhc3BIgoeZBZK-rfmJzG4td~0gM17hQjdKRRMGaphr~pvCidCAuGeQRSHBJE1rMTMSWCcn1mZUIVxnjkmh6RrqBx2MvxVFQZoPFM-qoP0U-hY48A5-O5fmDs3Jj40kkc5tKQivc-K00zzHTrIdc8x6T9ieeFypnSD6Uzbq4OEj6xU93SnNTtxbbkye3gYZaJ8GatEFFpy80FQRDYEKNoY~NRJTwPYwIuh1rg__",
-    flag: "🇬🇧",
     alt: "London Tower Bridge at sunset",
   },
   {
     title: "Study In The US",
-    image: "https://s3-alpha-sig.figma.com/img/65b3/d8b6/37aa0d2bcde6e2173e71da34a4271092?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=PAZK7bdU6gjkT3~OTqfayR4~oTWnz5h1CuCODyIe5eEfBE5dZqxLNKLVqCqbALz6PGPWfFlfHXVKSfw6P9mEkCF1j1F9D5GbQ~C2nyI3OUh9IPONkaAhc3BIgoeZBZK-rfmJzG4td~0gM17hQjdKRRMGaphr~pvCidCAuGeQRSHBJE1rMTMSWCcn1mZUIVxnjkmh6RrqBx2MvxVFQZoPFM-qoP0U-hY48A5-O5fmDs3Jj40kkc5tKQivc-K00zzHTrIdc8x6T9ieeFypnSD6Uzbq4OEj6xU93SnNTtxbbkye3gYZaJ8GatEFFpy80FQRDYEKNoY~NRJTwPYwIuh1rg__",
-    flag: "🇺🇸",
+    image: "https://s3-alpha-sig.figma.com/img/74ef/305d/f33593f2b6e21951d5b407e9da438c5e?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=lXD861iTSX0EtfA-sM~sYj3WSMMyGeXxZ2XoC40NHKuyxUTwJG~HxnlJh2HdDZ3OTKlRanRgLgVKiOoMY0rwpe69kc-hR-EEYuofgjzf2nFNAPatQC6R9FbWMbejw5LJnFi-foYav0Q9Kctfx0fbLzoKAl~jPrn3TqzNayudtRd1TbvWMQJt8t8LQdgPnjjX3AJrlGs57pP51HBJ04RBaeaTM3yvEhok0mt8t09MYTuucJtVB1jGsHIUlLjmmNYKyGQ6~LMyFdshfQw4pQjTp3QSxhH3KsSzoz6cQbGe0EsoLqKgWRdzqfz4x7eHNrAFK4eYwd2u3OBlO3-9CksyRQ__",
     alt: "Statue of Liberty and New York City skyline",
   },
   {
     title: "Study In Australia",
-    image: "https://s3-alpha-sig.figma.com/img/65b3/d8b6/37aa0d2bcde6e2173e71da34a4271092?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=PAZK7bdU6gjkT3~OTqfayR4~oTWnz5h1CuCODyIe5eEfBE5dZqxLNKLVqCqbALz6PGPWfFlfHXVKSfw6P9mEkCF1j1F9D5GbQ~C2nyI3OUh9IPONkaAhc3BIgoeZBZK-rfmJzG4td~0gM17hQjdKRRMGaphr~pvCidCAuGeQRSHBJE1rMTMSWCcn1mZUIVxnjkmh6RrqBx2MvxVFQZoPFM-qoP0U-hY48A5-O5fmDs3Jj40kkc5tKQivc-K00zzHTrIdc8x6T9ieeFypnSD6Uzbq4OEj6xU93SnNTtxbbkye3gYZaJ8GatEFFpy80FQRDYEKNoY~NRJTwPYwIuh1rg__",
-    flag: "🇦🇺",
+    image: "https://s3-alpha-sig.figma.com/img/97f5/49a0/66dd86c737eabe8bdf7dc2490d9d688a?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=FdSaUNZHdiz0urEdUXJ35yM8tZ64WGYcY5NO04CBprNgRe30oOCpYS~tvhR-tQMmIwqWNEDdRATuRoxZzUou~D-4Y0jGIZGQSudLbj6fdkGwS-RVBr0aLvqKxerULmOL89u9k3JwkLDVlijpKxveKUx0mtuJsneDiuhgUuOCuJlMu-4iYm-mPafjaPjoe0ny0gBvHMnCQepas1pIlsyZb~Fv5GraSlFlS8dpoCIqRMVxc9KDhOFbMFTXT3CZWoc1uCb8aV1FmqdRE-41D~yH0a7qJ18SJKEeZw9oL8wU8qB~VIXQ89CAooj~eAGZWOL~7ntXbCExGpYDzm8WikA2Tg__",
     alt: "Sydney Opera House and Harbour Bridge",
   },
   {
     title: "Study In Ireland",
-    image: "https://s3-alpha-sig.figma.com/img/65b3/d8b6/37aa0d2bcde6e2173e71da34a4271092?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=PAZK7bdU6gjkT3~OTqfayR4~oTWnz5h1CuCODyIe5eEfBE5dZqxLNKLVqCqbALz6PGPWfFlfHXVKSfw6P9mEkCF1j1F9D5GbQ~C2nyI3OUh9IPONkaAhc3BIgoeZBZK-rfmJzG4td~0gM17hQjdKRRMGaphr~pvCidCAuGeQRSHBJE1rMTMSWCcn1mZUIVxnjkmh6RrqBx2MvxVFQZoPFM-qoP0U-hY48A5-O5fmDs3Jj40kkc5tKQivc-K00zzHTrIdc8x6T9ieeFypnSD6Uzbq4OEj6xU93SnNTtxbbkye3gYZaJ8GatEFFpy80FQRDYEKNoY~NRJTwPYwIuh1rg__",
-    flag: "🇮🇪",
+    image: "https://s3-alpha-sig.figma.com/img/3165/0727/872ef7e2f1f010d4c0cb798a420f96a5?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=aRvYnAeJ41FLTnrggMoIh2Qvtz-ZrUZRWdwbd1Wy6Z2v3uWDZwoaFE4gEcuAoEqnUphkpQ-eoZx7ct~fY88Jf4Pio~MF3wJ~Woonk~L~iGcO7GTCAdY~fJM3qq1pCfqS~DcJvA-dEYsj6K~mZmdCLLBLlyrmtQpZZcbYGRQRsfPW1jDVSd5CDmvpukAGP~ezAA60H~DtfGuXiOdM2DLRwqplPTN-HbPJhn7t4nkW1bwS5O~Wo7MHub9UUpvjkyM1QaDCsk19dK~PlTpZs6WBYVIJmTkGP10LnJxUIm1Yjyols0UDwdEjxp7Nn8n78owKepOn3nFMjFb5TyxM2qS3ZQ__",
     alt: "Cliffs of Moher in Ireland",
   },
 ]
